7b1ec89118704e22c4dcd95ac4b4d857e3b79abf
Nicholas Long <nicholas.long@nrel.gov>
2014-05-15 11:30:19 -0600

    reformat to match ruby style guide and move to .erb instead of .in

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-openstudio-threejs
-==================
+# OpenStudio-ThreeJS
+
+To run this example simply call
+
+    ruby measure.rb /path/to/model.osm
+    ruby measure.rb /path/to/model.idf
+    ruby measure.rb /path/to/model.xml
+
+It will simply run the generator on the model that is passed in
--- render.rb
+++ render.rb
@@ -4,45 +4,48 @@ require 'erb'
 
 model = nil
 if ARGV[0].nil?
-  #model = OpenStudio::Model::exampleModel
+  # generate a simple model for testing purposes
 
   model = OpenStudio::Model::Model.new
-  space = OpenStudio::Model::Space.new(model)
-  
+  # space not used
+  # space = OpenStudio::Model::Space.new(model)
+
   verts = OpenStudio::Point3dVector.new
-  verts << OpenStudio::Point3d.new(0,0,0)
-  verts << OpenStudio::Point3d.new(0,10,0)
-  verts << OpenStudio::Point3d.new(10,10,0)
-  verts << OpenStudio::Point3d.new(10,0,0)
-  floor = OpenStudio::Model::Surface.new(verts, model)
-  
+  verts << OpenStudio::Point3d.new(0, 0, 0)
+  verts << OpenStudio::Point3d.new(0, 10, 0)
+  verts << OpenStudio::Point3d.new(10, 10, 0)
+  verts << OpenStudio::Point3d.new(10, 0, 0)
+
+  # floor not used
+  # floor = OpenStudio::Model::Surface.new(verts, model)
+
   verts = OpenStudio::Point3dVector.new
-  verts << OpenStudio::Point3d.new(10,0,10)
-  verts << OpenStudio::Point3d.new(10,0,0)
-  verts << OpenStudio::Point3d.new(10,10,0)
-  verts << OpenStudio::Point3d.new(10,10,10)
+  verts << OpenStudio::Point3d.new(10, 0, 10)
+  verts << OpenStudio::Point3d.new(10, 0, 0)
+  verts << OpenStudio::Point3d.new(10, 10, 0)
+  verts << OpenStudio::Point3d.new(10, 10, 10)
   wall = OpenStudio::Model::Surface.new(verts, model)
 else
   puts ARGV[0]
   extension = File.extname(ARGV[0])
-  inputPath = OpenStudio::Path.new(ARGV[0])
-  if extension == ".osm"
+  input_path = OpenStudio::Path.new(ARGV[0])
+  if extension == '.osm'
     vt = OpenStudio::OSVersion::VersionTranslator.new
-    model = vt.loadModel(inputPath).get
-  elsif extension == ".idf"
+    model = vt.loadModel(input_path).get
+  elsif extension == '.idf'
     rt = OpenStudio::EnergyPlus::ReverseTranslator.new
-    model = rt.loadModel(inputPath).get  
-  elsif extension == ".xml"
+    model = rt.loadModel(input_path).get
+  elsif extension == '.xml'
     # try sdd
     rt = OpenStudio::SDD::SddReverseTranslator.new
-    model = rt.loadModel(inputPath)
-    if not model.empty?
+    model = rt.loadModel(input_path)
+    if !model.empty?
       model = model.get
     elsif model.empty?
       # try gbXML
       rt = OpenStudio::GbXML::GbXMLReverseTranslator.new
-      model = rt.loadModel(inputPath)
-      if not model.empty?
+      model = rt.loadModel(input_path)
+      unless model.empty?
         model = model.get
       end
     end
@@ -61,129 +64,129 @@ z[0] = 0
 z[1] = 0
 z[2] = 1
 
-def getVertexIndex(vertex, allVertices, sceneGeometry, tol = 0.001)
-  allVertices.each_index do |i|
-    if OpenStudio::getDistance(vertex, allVertices[i]) < tol
+def get_vertex_index(vertex, all_vertices, scene_geometry, tol = 0.001)
+  all_vertices.each_index do |i|
+    if OpenStudio.getDistance(vertex, all_vertices[i]) < tol
       return i
     end
   end
-  sceneGeometry.insert(-1,"geometry.vertices.push( new THREE.Vector3( #{vertex.x}, #{vertex.z}, #{-vertex.y} ) );\n")
-  allVertices << vertex
-  return (allVertices.length - 1)
+  scene_geometry.insert(-1, "geometry.vertices.push( new THREE.Vector3( #{vertex.x}, #{vertex.z}, #{-vertex.y} ) );\n")
+  all_vertices << vertex
+  (all_vertices.length - 1)
 end
 
-allVertices = []
+all_vertices = []
 
-sceneGeometry = "geometry = new THREE.Geometry();\n"
-sceneGeometry += "var face;\n"
-sceneGeometry += "geometry.materials = materials;\n"
+scene_geometry = "geometry = new THREE.Geometry();\n"
+scene_geometry += "var face;\n"
+scene_geometry += "geometry.materials = materials;\n"
 
-objVertices = ""
-objFaces = ""
+obj_vertices = ''
+obj_faces = ''
 
 model.getSurfaces.each do |surface|
 
   ext_color = nil
   int_color = nil
-  surfaceType = surface.surfaceType.upcase
-  if surfaceType == "FLOOR"
-    ext_color = "floor_ext_index"
-    int_color = "floor_int_index"
-  elsif surfaceType == "WALL"
-    ext_color = "wall_ext_index"
-    int_color = "wall_int_index"  
-  elsif surfaceType == "ROOFCEILING"
-    ext_color = "roof_ext_index"
-    int_color = "roof_int_index"    
-  end
-  
-  surfaceVertices = surface.vertices
-  t = OpenStudio::Transformation::alignFace(surfaceVertices)
-  r = t.rotationMatrix
-  tInv = t.inverse
-  
-  siteTransformation = OpenStudio::Transformation.new
-  planarSurfaceGroup = surface.planarSurfaceGroup
-  if not planarSurfaceGroup.empty?
-    siteTransformation = planarSurfaceGroup.get.siteTransformation
-  end
-  
-  surfaceVertices = tInv*surfaceVertices
-  subSurfaceVertices = OpenStudio::Point3dVectorVector.new
-  subSurfaces = surface.subSurfaces
-  subSurfaces.each do |subSurface|
-    subSurfaceVertices << tInv*subSurface.vertices
+  surface_type = surface.surfaceType.upcase
+  if surface_type == 'FLOOR'
+    ext_color = 'floor_ext_index'
+    int_color = 'floor_int_index'
+  elsif surface_type == 'WALL'
+    ext_color = 'wall_ext_index'
+    int_color = 'wall_int_index'
+  elsif surface_type == 'ROOFCEILING'
+    ext_color = 'roof_ext_index'
+    int_color = 'roof_int_index'
   end
 
-  triangles = OpenStudio::computeTriangulation(surfaceVertices, subSurfaceVertices)
-  if triangles.empty?
-    puts "Failed to triangulate surface #{surface.name} with #{subSurfaces.size} sub surfaces"
+  surface_vertices = surface.vertices
+  t = OpenStudio::Transformation.alignFace(surface_vertices)
+  r = t.rotationMatrix
+  t_inv = t.inverse
+
+  site_transformation = OpenStudio::Transformation.new
+  planar_surface_group = surface.planarSurfaceGroup
+  unless planar_surface_group.empty?
+    siteTransformation = planar_surface_group.get.siteTransformation
   end
-  
+
+  surface_vertices = t_inv * surface_vertices
+  sub_surface_vertices = OpenStudio::Point3dVectorVector.new
+  sub_surfaces = surface.subSurfaces
+  sub_surfaces.each do |sub_surface|
+    sub_surface_vertices << t_inv * sub_surface.vertices
+  end
+
+  triangles = OpenStudio.computeTriangulation(surface_vertices, sub_surface_vertices)
+  if triangles.empty?
+    puts "Failed to triangulate surface #{surface.name} with #{sub_surfaces.size} sub surfaces"
+  end
+
   triangles.each do |vertices|
-    vertices = siteTransformation*t*vertices
-    normal = siteTransformation.rotationMatrix*r*z
+    vertices = site_transformation * t * vertices
+    normal = site_transformation.rotationMatrix * r * z
 
     indices = []
     vertices.each do |vertex|
-      indices << getVertexIndex(vertex, allVertices, sceneGeometry)  
+      indices << get_vertex_index(vertex, all_vertices, scene_geometry)
     end
-    sceneGeometry += "face = new THREE.Face3( #{indices.join(', ')}, new THREE.Vector3( #{normal[0]}, #{normal[2]}, #{-normal[1]} ));\n"
-    sceneGeometry += "face.materialIndex = #{ext_color};\n";
-    sceneGeometry += "face.castShadow = true;\n";
-    sceneGeometry += "face.receiveShadow  = true;\n";
-    sceneGeometry += "geometry.faces.push(face);\n"
-    
-    sceneGeometry += "face = new THREE.Face3( #{indices.join(', ')}, new THREE.Vector3( #{normal[0]}, #{normal[2]}, #{-normal[1]} ));\n"
-    sceneGeometry += "face.materialIndex = #{int_color};\n";
-    sceneGeometry += "face.castShadow = true;\n";
-    sceneGeometry += "face.receiveShadow  = true;\n";
-    sceneGeometry += "geometry.faces.push(face);\n"
-    
-    indices.each_index {|i| indices[i] = indices[i] + 1}
-    objFaces += "f #{indices.join(' ')}\n"
+    scene_geometry += "face = new THREE.Face3( #{indices.join(', ')}, new THREE.Vector3( #{normal[0]}, #{normal[2]}, #{-normal[1]} ));\n"
+    scene_geometry += "face.materialIndex = #{ext_color};\n"
+    scene_geometry += "face.castShadow = true;\n"
+    scene_geometry += "face.receiveShadow  = true;\n"
+    scene_geometry += "geometry.faces.push(face);\n"
+
+    scene_geometry += "face = new THREE.Face3( #{indices.join(', ')}, new THREE.Vector3( #{normal[0]}, #{normal[2]}, #{-normal[1]} ));\n"
+    scene_geometry += "face.materialIndex = #{int_color};\n"
+    scene_geometry += "face.castShadow = true;\n"
+    scene_geometry += "face.receiveShadow  = true;\n"
+    scene_geometry += "geometry.faces.push(face);\n"
+
+    indices.each_index { |i| indices[i] = indices[i] + 1 }
+    obj_faces += "f #{indices.join(' ')}\n"
   end
-  
-  subSurfaces.each do |subSurface|
- 
-    subSurfaceVertices = tInv*subSurface.vertices
-    triangles = OpenStudio::computeTriangulation(subSurfaceVertices, OpenStudio::Point3dVectorVector.new)
+
+  sub_surfaces.each do |sub_surface|
+
+    sub_surface_vertices = t_inv * sub_surface.vertices
+    triangles = OpenStudio.computeTriangulation(sub_surface_vertices, OpenStudio::Point3dVectorVector.new)
 
     triangles.each do |vertices|
-      vertices = siteTransformation*t*vertices
-      normal = siteTransformation.rotationMatrix*r*z
+      vertices = site_transformation * t * vertices
+      normal = site_transformation.rotationMatrix * r * z
 
       indices = []
       vertices.each do |vertex|
-        indices << getVertexIndex(vertex, allVertices, sceneGeometry)  
-      end    
-      
-      sceneGeometry += "face = new THREE.Face3( #{indices.join(', ')}, new THREE.Vector3( #{normal[0]}, #{normal[2]}, #{-normal[1]} ));\n"
-      sceneGeometry += "face.materialIndex = window_ext_index;\n";
-      sceneGeometry += "face.castShadow = true;\n";
-      sceneGeometry += "face.receiveShadow  = true;\n";
-      sceneGeometry += "geometry.faces.push(face);\n"
-      
-      sceneGeometry += "face = new THREE.Face3( #{indices.join(', ')}, new THREE.Vector3( #{normal[0]}, #{normal[2]}, #{-normal[1]} ));\n"
-      sceneGeometry += "face.materialIndex = window_int_index;\n";
-      sceneGeometry += "face.castShadow = true;\n";
-      sceneGeometry += "face.receiveShadow  = true;\n";
-      sceneGeometry += "geometry.faces.push(face);\n"
-      
-      indices.each_index {|i| indices[i] = indices[i] + 1}
-      objFaces += "f #{indices.join(' ')}\n"
+        indices << get_vertex_index(vertex, all_vertices, scene_geometry)
+      end
+
+      scene_geometry += "face = new THREE.Face3( #{indices.join(', ')}, new THREE.Vector3( #{normal[0]}, #{normal[2]}, #{-normal[1]} ));\n"
+      scene_geometry += "face.materialIndex = window_ext_index;\n"
+      scene_geometry += "face.castShadow = true;\n"
+      scene_geometry += "face.receiveShadow  = true;\n"
+      scene_geometry += "geometry.faces.push(face);\n"
+
+      scene_geometry += "face = new THREE.Face3( #{indices.join(', ')}, new THREE.Vector3( #{normal[0]}, #{normal[2]}, #{-normal[1]} ));\n"
+      scene_geometry += "face.materialIndex = window_int_index;\n"
+      scene_geometry += "face.castShadow = true;\n"
+      scene_geometry += "face.receiveShadow  = true;\n"
+      scene_geometry += "geometry.faces.push(face);\n"
+
+      indices.each_index { |i| indices[i] = indices[i] + 1 }
+      obj_faces += "f #{indices.join(' ')}\n"
     end
   end
 
 end
-sceneGeometry += "mesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(geometry.materials) );\n"
-sceneGeometry += "mesh.castShadow = true;\n"
-sceneGeometry += "mesh.receiveShadow = true;\n"
-sceneGeometry += "scene.add( mesh );\n\n"
-    
+scene_geometry += "mesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(geometry.materials) );\n"
+scene_geometry += "mesh.castShadow = true;\n"
+scene_geometry += "mesh.receiveShadow = true;\n"
+scene_geometry += "scene.add( mesh );\n\n"
+
 # read in template
-html_in_path = "#{File.dirname(__FILE__)}/render.html.in"
-html_in = ""
+html_in_path = "#{File.dirname(__FILE__)}/resources/render.html.erb"
+html_in = ''
 File.open(html_in_path, 'r') do |file|
   html_in = file.read
 end
@@ -193,11 +196,11 @@ renderer = ERB.new(html_in)
 html_out = renderer.result(binding)
 
 # write html file
-html_out_path = "./render.html"
+html_out_path = 'render.html'
 File.open(html_out_path, 'w') do |file|
   file << html_out
-  
-  # make sure data is written to the disk one way or the other      
+
+  # make sure data is written to the disk one way or the other
   begin
     file.fsync
   rescue
@@ -206,16 +209,16 @@ File.open(html_out_path, 'w') do |file|
 end
 
 # write object file
-html_out_path = "./render.obj"
+html_out_path = 'render.obj'
 File.open(html_out_path, 'w') do |file|
 
   file << "# OpenStudio OBJ Export\n"
-  allVertices.each do |v|
+  all_vertices.each do |v|
     file << "v #{v.x} #{v.z} #{-v.y}\n"
   end
-  file << objFaces
-  
-  # make sure data is written to the disk one way or the other      
+  file << obj_faces
+
+  # make sure data is written to the disk one way or the other
   begin
     file.fsync
   rescue
